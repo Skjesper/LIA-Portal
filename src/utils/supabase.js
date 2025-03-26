5c4b625aa564fcde2174fa1ba9c43da05cb40234
Skjesper <jesperskeppstedt@gmail.com>
2025-03-26 13:25:14 +0100

    fixed supabase issues

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,8 @@
+// src/utils/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://upqcpapehcoykqzwavpa.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY // Match the name from your .env file
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
--- src/utils/supabase/server.js
+++ src/utils/supabase/server.js
@@ -1,29 +1,34 @@
-import { createServerClient } from '@supabase/ssr'
-  import { cookies } from 'next/headers'
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-  export async function createClient() {
-    const cookieStore = await cookies()
-
-    return createServerClient(
-      process.env.local.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.local.NEXT_PUBLIC_SUPABASE_KEY,
-      {
-        cookies: {
-          getAll() {
-            return cookieStore.getAll()
-          },
-          setAll(cookiesToSet) {
-            try {
-              cookiesToSet.forEach(({ name, value, options }) =>
-                cookieStore.set(name, value, options)
-              )
-            } catch {
-              // The `setAll` method was called from a Server Component.
-              // This can be ignored if you have middleware refreshing
-              // user sessions.
-            }
-          },
+export const createClient = () => {
+  const cookieStore = cookies();
+  
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value;
         },
-      }
-    )
-  }
+        set(name, value, options) {
+          try {
+            cookieStore.set(name, value, options);
+          } catch {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name, options) {
+          try {
+            cookieStore.set(name, '', { ...options, maxAge: 0 });
+          } catch {
+            // The `remove` method was called from a Server Component.
+          }
+        },
+      },
+    },
+  );
+};
