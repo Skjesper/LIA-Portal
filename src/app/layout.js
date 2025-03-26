c2b81c7a46a7afd319cfe71536286f2706cfdafd
Skjesper <jesperskeppstedt@gmail.com>
2025-03-26 13:56:49 +0100

    created style components.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/components/styles/globals.css";
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
