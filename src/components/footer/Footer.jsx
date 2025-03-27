482c1ce10173b7864956f3ae97e1d9b42acae70e
Skjesper <jesperskeppstedt@gmail.com>
2025-03-27 10:53:14 +0100

    created a footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,20 @@
 import styles from './Footer.module.css';
+import Link from 'next/link';
+
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Footer placeholder</h1>
+      <div className={styles.leftContainer}>
+      <Link href="/students" className={styles.link}>
+            Studerande
+          </Link>
+          <Link href="/companies" className={styles.link}>
+            Företag
+          </Link>
+          <Link href="/event" className={styles.link}>
+            Event
+          </Link>
         <p className={styles.text}>© 2025 Ditt företag</p>
       </div>
     </footer>
