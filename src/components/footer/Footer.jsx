b3491fc99390d7689de844b1995ba93b9952b589
Skjesper <jesperskeppstedt@gmail.com>
2025-03-27 11:45:42 +0100

    added some new directories. Progress on the footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,22 +1,35 @@
 import styles from './Footer.module.css';
 import Link from 'next/link';
-
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.leftContainer}>
-      <Link href="/students" className={styles.link}>
-            Studerande
-          </Link>
-          <Link href="/companies" className={styles.link}>
-            Företag
-          </Link>
-          <Link href="/event" className={styles.link}>
-            Event
-          </Link>
-        <p className={styles.text}>© 2025 Ditt företag</p>
+      <div className={styles.Container}>
+        <Image
+          src="/logos/white-main.svg"
+          alt="Huvudlogotyp"
+          width={90}
+          height={30}
+        />
+        <Link href="/students" className={styles.link}>
+          Studerande
+        </Link>
+        <Link href="/companies" className={styles.link}>
+          Företag
+        </Link>
+        <Link href="/event" className={styles.link}>
+          Event
+        </Link>
       </div>
+      
+      <Image 
+        src="/logos/yrgo-logo-white.svg"
+        alt="Yrgo logotyp"
+        width={120}
+        height={40}
+        className={styles.logo}
+      />
     </footer>
   );
 }
