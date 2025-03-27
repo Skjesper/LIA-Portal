45f5d0a3b03f018c4cb90c300b9c6d5ae29b8b15
Skjesper <jesperskeppstedt@gmail.com>
2025-03-27 14:29:30 +0100

    (Maybe changed global font) and done with the footer for mobile

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google';
 import "@/components/styles/globals.css";
 import "@/components/styles/colors.css";
 import Header from '@/components/Header'
 import Footer from '@/components/footer/Footer'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -22,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inter.variable} ${inter.variable}`}>
       <Header />
         {children}
         <Footer />
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,37 +6,42 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.Nav}>
-      <div className={styles.logoContainer}>
-    <Image
-      src="/logos/white-main.svg"
-      alt="Huvudlogotyp"
-      width={130}
-      height={45}
-    />
-  </div>
-        
-      <div className={styles.NavLinks}>
-        <Link href="/students" className={styles.link}>
-          Studerande
-        </Link>
-        <Link href="/companies" className={styles.link}>
-          Företag
-        </Link>
-        <Link href="/event" className={styles.link}>
-          Event
-        </Link>
+        <div className={styles.logoContainer}>
+          <Image
+            src="/logos/white-main.svg"
+            alt="Huvudlogotyp"
+            width={130}
+            height={45}
+          />
         </div>
-      
+        
+        <div className={styles.NavLinks}>
+          <Link href="/students" className={styles.link}>
+            Studerande
+          </Link>
+          <Link href="/companies" className={styles.link}>
+            Företag
+          </Link>
+          <Link href="/event" className={styles.link}>
+            Event
+          </Link>
+        </div>
       </div>
-      <div className={styles.rightContainer}>
       
-      <Image 
-        src="/logos/yrgo-logo-white.svg"
-        alt="Yrgo logotyp"
-        width={120}
-        height={40}
-        className={styles.logo}
-      />
+      <div className={styles.login}>
+        <Link href="/login" className={styles.link}>
+          Logga in
+        </Link>
+      </div>
+      
+      <div className={styles.rightContainer}>
+        <Image 
+          src="/logos/yrgo-logo-white.svg"
+          alt="Yrgo logotyp"
+          width={120}
+          height={40}
+          className={styles.logo}
+        />
       </div>
     </footer>
   );
