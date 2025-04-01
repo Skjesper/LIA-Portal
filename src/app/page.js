1da4c1b5174f40f2f2b32ea3ae78df7d10e83334
Skjesper <jesperskeppstedt@gmail.com>
2025-04-01 14:46:27 +0200

    all button components are done

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,15 @@ export default function Home() {
     <Button className={styles.filledBlack}>filled-black</Button>
     <Button className={styles.warning}>warning</Button>
     <Button className={styles.success}>warning</Button>
+    <Button className={styles.filledWhite}>filledWhite</Button>
     <Button className={styles.warning}>warning</Button>
-    <Button className={styles.warning}>warning</Button>
-    
+    <h2>Fokuserad knapp</h2>
+      <Button className={styles.unfilledBlack}>Fokuserad</Button>
+      <Button className={styles.unfilledWhite}>unfilledWhite</Button>
+      <Button className={styles.unfilledBlack}>unfilledBlack</Button>
+      <Button className={styles.underlinedWhite}>
+     Button</Button>
+      <Button className={styles.underlinedBlack}>Button</Button>
     
   </div>
   );
