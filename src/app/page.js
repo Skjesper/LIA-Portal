9816a98a1e86238193803854dd61aba36009ecb8
Skjesper <jesperskeppstedt@gmail.com>
2025-04-01 12:07:53 +0200

    fixed import for button

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 
 import Link from 'next/link';
 import Button from '@/components/ui/Button/Button.jsx';
+import styles from '@/components/ui/Button/Button.module.css'; // Lägg till denna import
+
 
 export default function Home() {
   return (
-    <div className="flex gap-2 flex-wrap">
-    <Button variant="filled-black">filled-black</Button>
-    <Button variant="filled-white">filled-white</Button>
-    <Button variant="warning">Warning</Button>
-    <Button variant="success">Success</Button>
+    <div>
+    <Button className={styles.filledBlack}>filled-black</Button>
+    
     
     
   </div>
--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,10 +1,13 @@
+
 // Button.jsx
 import React from 'react';
 import styles from '@/components/ui/Button/Button.module.css';
 
+
 const Button = ({ children, className, ...props }) => {
-  // Kombinera 'btn' med eventuella andra klasser
-  const buttonClassName = className ? `btn ${className}` : 'btn';
+  // Använd styles.button från CSS-modulen som basklass
+  // Observera att vi använder styles.button här, baserat på vanliga namnkonventioner för CSS-moduler
+  const buttonClassName = className ? `${styles.button} ${className}` : styles.button;
   
   return (
     <button className={buttonClassName} {...props}>
