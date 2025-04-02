5c5e88986250f62e609931bb32fbe529266b1dee
Skjesper <jesperskeppstedt@gmail.com>
2025-04-02 13:16:16 +0200

    Solved an issue where some styles overwrote others

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       <Section>
         <div className={pageStyles.applyWrapper}>
           <h2>01/Anmälan</h2>
-          <p>Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. Semper nun sodales malesuada. Non habitant.</p>
+          <p className={pageStyles.text}>Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. Semper nun sodales malesuada. Non habitant.</p>
           <RSVPCard></RSVPCard>
         </div>
       </Section>
@@ -23,8 +23,8 @@ export default function Home() {
         <div className={pageStyles.applyWrapper}>
           <h2>02/Företag</h2>
 
-          <p>Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. Semper nun sodales malesuada. Non habitant.</p>
-          <p>Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. Semper nun sodales malesuada. Non habitant.</p>
+          <p className={pageStyles.text}>Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. Semper nun sodales malesuada. Non habitant.</p>
+          <p className={pageStyles.text}>Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. Semper nun sodales malesuada. Non habitant.</p>
           <Button className={buttonStyles.underlinedBlack}>Se alla företag</Button>
           <CompanyCard></CompanyCard>
         </div>
@@ -32,12 +32,11 @@ export default function Home() {
       <Section>
         <div className={pageStyles.applyWrapper}>
           <h2>03/Studenter</h2>
-          <p>Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. Semper nun sodales malesuada. Non habitant.</p>
+          <p className={pageStyles.text}>Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. Semper nun sodales malesuada. Non habitant.</p>
           <p>Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. Semper nun sodales malesuada. Non habitant.</p>
           <Button className={buttonStyles.underlinedBlack}>Se alla studenter</Button>
         </div>
       </Section>
-      <Label className={labelStyles.filled}>Hej</Label>
     </main>
   );
 }
--- src/components/ui/CompanyCard/CompanyCard.jsx
+++ src/components/ui/CompanyCard/CompanyCard.jsx
@@ -1,5 +1,6 @@
 // CompanyCard.jsx
 import React from 'react';
+import Image from 'next/image';
 import styles from '@/components/ui/CompanyCard/CompanyCard.module.css';
 import Button, { buttonStyles } from '@/components/ui/Button/Button';
 
@@ -9,6 +10,20 @@ const CompanyCard = ({ children, className, ...props }) => {
   return (
     <div className={companyCardClassName} {...props}>
       {children}
+      <section className={styles.titleContainer}>
+      <div className={styles.title}>FÖRETAGSNAMN</div>
+      <div className={styles.imageContainer}>
+          <Image
+            src="/logos/white-main.svg"
+            alt="Company Logo"
+            width={100}
+            height={100}
+            className={styles.image}
+          />
+        </div>
+      </section>
+      <p className={styles.companyCard}>Lorem ipsum dolor sit amet consectetur. Pulvinar tempus mi congue elementum lectus. Odio pellentesque posuere sapien enim at lacus.</p>
+      <Button className={buttonStyles.underlinedWhite} type="submit">Om företaget</Button>
     </div>
   );
 };
