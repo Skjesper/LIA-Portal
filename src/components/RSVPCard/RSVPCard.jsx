730ce19156c06736b183a7c12dd8ad0f456e7a91
Skjesper <jesperskeppstedt@gmail.com>
2025-04-02 22:40:41 +0200

    updated section module

--- src/components/RSVPCard/RSVPCard.jsx
+++ src/components/RSVPCard/RSVPCard.jsx
@@ -62,74 +62,83 @@ const RSVPCard = ({ title, onSubmit }) => {
   };
 
   return (
-    <div className={styles.rsvpCard}>
-      <div className={styles.titleWrapper}>
-      <div className={styles.title}>ANMÄLAN TILL EVENT 23/4</div>
-      
-      <Image
+    <div className={styles.rsvpWrapper}>
+      <section className={styles.rsvpCard}>
+        <div className={styles.titleWrapper}>
+          <div className={styles.title}>ANMÄLAN TILL EVENT 23/4</div>
+          
+          <Image
             src="/logos/white-main.svg"
             alt="Company Logo"
             width={100}
             height={100}
             className={styles.image}
           />
-      </div>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.formGroup}>
-          <label htmlFor="companyName" className={styles.label}>
-            *FÖRETAGSNAMN
-          </label>
-          <input
-            type="text"
-            id="companyName"
-            name="companyName"
-            placeholder="ex. Företag AB"
-            value={formData.companyName}
-            onChange={handleChange}
-            className={styles.input}
-            required
-          />
+
+
         </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="email" className={styles.label}>
-            *EMAIL
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="exempel@mail.com"
-            value={formData.email}
-            onChange={handleChange}
-            className={styles.input}
-            required
-          />
-        </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="participants" className={styles.label}>
-            *HUR MÅNGA ÄR NI SOM VILL DELTA?
-          </label>
-          <input
-            type="number"
-            id="participants"
-            name="participants"
-            placeholder="ex. 2"
-            value={formData.participants}
-            onChange={handleChange}
-            className={styles.input}
-            min="1"
-            required
-          />
-        </div>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.formGroup}>
+            <label htmlFor="companyName" className={styles.label}>
+              *FÖRETAGSNAMN
+            </label>
+            <input
+              type="text"
+              id="companyName"
+              name="companyName"
+              placeholder="ex. Företag AB"
+              value={formData.companyName}
+              onChange={handleChange}
+              className={styles.input}
+              required
+            />
+          </div>
 
-        <div className={styles.buttonContainer}>
-        <Button type="submit" className={buttonStyles.filledWhite}>
-        ANMÄL TILL EVENT
-        </Button>
-        </div>
-      </form>
+          <div className={styles.formGroup}>
+            <label htmlFor="email" className={styles.label}>
+              *EMAIL
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="exempel@mail.com"
+              value={formData.email}
+              onChange={handleChange}
+              className={styles.input}
+              required
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label htmlFor="participants" className={styles.label}>
+              *HUR MÅNGA ÄR NI SOM VILL DELTA?
+            </label>
+            <input
+              type="number"
+              id="participants"
+              name="participants"
+              placeholder="ex. 2"
+              value={formData.participants}
+              onChange={handleChange}
+              className={styles.input}
+              min="1"
+              required
+            />
+          </div>
+
+          <div className={styles.buttonContainer}>
+            <Button type="submit" className={buttonStyles.filledWhite}>
+              ANMÄL TILL EVENT
+            </Button>
+          </div>
+        </form>
+        
+      </section>
+      
+     
     </div>
   );
 };
