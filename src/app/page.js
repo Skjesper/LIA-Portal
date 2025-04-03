88492712b6e4f84172806b39ed11237a7bb676f0
Skjesper <jesperskeppstedt@gmail.com>
2025-04-03 11:34:13 +0200

    responsive section :)

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,9 @@ import pageStyles from './page.module.css'; // Importera page.module.css
 import Section from '@/components/Sections/Sections';
 import RSVPCard from '@/components/RSVPCard/RSVPCard';
 import Label, { labelStyles } from '@/components/ui/Label/Label';
-import CompanyCard from '@/components/ui/CompanyCard/CompanyCard';
-import RandomCompanies from '@/components/ui/RandomCompanies';
+import RandomCompanies from '@/components/ui/CompanyCard/RandomCompanies';
+import CompanyCardSection from '@/components/ui/CompanyCard/CompanySection';
+
 
 export default function Home() {
   return (
@@ -24,35 +25,10 @@ export default function Home() {
       </Section>
 
       <Section>
-        <div className={pageStyles.applyWrapper}>
-          <h2>02/Företag</h2>
-          <p className={pageStyles.text}>
-            Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. 
-            Semper nun sodales malesuada. Non habitant.
-          </p>
-          <p className={pageStyles.text}>
-            Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. 
-            Semper nun sodales malesuada. Non habitant.
-          </p>
-          <Button className={buttonStyles.underlinedBlack}>Se alla företag</Button>
-          <RandomCompanies />
-        </div>
+     
+      <CompanyCardSection />
       </Section>
-
-      <Section>
-        <div className={pageStyles.applyWrapper}>
-          <h2>03/Studenter</h2>
-          <p className={pageStyles.text}>
-            Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. 
-            Semper nun sodales malesuada. Non habitant.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet si consectetur. Sagittis faucibus vita in faucibus nunc. 
-            Semper nun sodales malesuada. Non habitant.
-          </p>
-          <Button className={buttonStyles.underlinedBlack}>Se alla studenter</Button>
-        </div>
-      </Section>
+      
     </main>
   );
 }
