5790e6698c9445ad832207c1fee1cfe1672df2f1
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-04-15 23:03:30 +0200

    fixed footer styling on desktop and re-exported logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,65 +10,62 @@ export default function Footer() {
 
   const isMobile = useBreakpoint(768);
 
-  if (!isMobile) return (
+  if(!isMobile) return (
     <footer className={styles.footer}>
-    <div className={styles.logoContainer}>
-      <Image
-        src="/logos/white-main.svg"
-        alt="Huvudlogotyp"
-        width={130}
-        height={45}
-      />
-    </div>
+      <div className={styles.logoContainer}>
+        <Image
+          src="/logos/white-main.svg"
+          alt="Huvudlogotyp"
+          className={styles.logoContainerImg}
+          width={243}
+          height={221}
+        />
+        <nav className={styles.Nav}>
+            <div className={styles.linkGroupOne}>
+              <Link href="/event" className={styles.link}>
+                Event
+              </Link>
+              <Link href="/login" className={styles.link}>
+                <span className={buttonStyles.underlinedWhite}>Logga in</span>
+              </Link>
+            </div>
     
-    <nav className={styles.Nav}>
-      <div className={styles.NavLinks}>
-        <div className={styles.linkGroup}>
-          <Link href="/event" className={styles.link}>
-            Event
-          </Link>
-          <Link href="/login" className={styles.link}>
-            <span className={buttonStyles.underlinedWhite}>Logga in</span>
-          </Link>
-        </div>
-
-        <div className={styles.linkGroup}>
-          <Link href="/companies" className={styles.link}>
-            Företag
-          </Link>
-          <Link href="/students" className={styles.link}>
-            Studerande
-          </Link>
-        </div>
-
-        <div className={styles.linkGroup}>
-          <Link href="https://linkedin.com" className={styles.link}>
-            Linkedin
-          </Link>
-          <Link href="/event" className={styles.link}>
-            Event
-          </Link>
-        </div>
+            <div className={styles.linkGroupTwo}>
+              <Link href="/companies" className={styles.link}>
+                Företag
+              </Link>
+              <Link href="/students" className={styles.link}>
+                Studerande
+              </Link>
+            </div>
+    
+            <div className={styles.linkGroupThree}>
+              <Link href="https://linkedin.com" className={styles.link}>
+                Linkedin
+              </Link>
+              <Link href="/event" className={styles.link}>
+                Event
+              </Link>
+            </div>
+        </nav>
       </div>
-    </nav>
-    
-    <div className={styles.rightContainer}>
-      <Image 
-        src="/logos/yrgo-logo-white.svg"
-        alt="Yrgo logotyp"
-        width={120}
-        height={40}
-        className={styles.logo}
-      />
-    </div>
-  </footer>
+        <Image 
+          src="/logos/yrgo-logo-white-flipped.svg"
+          alt="Yrgo logotyp"
+          width={120}
+          height={40}
+          className={styles.logo}
+        />
+    </footer>
   );
+
   return (
     <footer className={styles.footer}>
       <div className={styles.logoContainer}>
         <Image
           src="/logos/white-main.svg"
           alt="Huvudlogotyp"
+          className={styles.logoContainerImg}
           width={130}
           height={45}
         />
