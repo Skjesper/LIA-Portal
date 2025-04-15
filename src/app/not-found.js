d8a468842d98705dca49051742b10cba12f04aba
Skjesper <jesperskeppstedt@gmail.com>
2025-04-15 20:32:09 +0200

    Create not-found.js

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div style={{ padding: '2rem' }}>
+        <h1>404 – Sidan kunde inte hittas</h1>
+        <p>Dubbelkolla att URL:en är korrekt.</p>
+      </div>
+    );
+  }
